b14f39156e889baae2a9c667279d714d08ecc73e
Matteo <matteo.tgn@gmail.com>
2023-07-16 21:20:52 +0200

    implementazione curtain

--- custom_components/buspro/pybuspro/devices/__init__.py
+++ custom_components/buspro/pybuspro/devices/__init__.py
@@ -2,6 +2,7 @@
 from .control import *
 from .device import Device
 from .light import Light
+from .curtain import Curtain
 from .scene import Scene
 from .sensor import Sensor
 from .switch import Switch
